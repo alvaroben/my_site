7b5be650a82911074021b061d7e16f4da2c291fb
Alvaro Bencosme <alvarobencosme@gmail.com>
2022-06-21 12:13:09 -0400

    first commit

---
+++ alvarobencosme_site/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-f$2xy6*h*5x-++uuq(ufjriz3z24&+$5mr2g^1#_vwv7xw3zxo"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
